1d49b72cb49fc6228fd084a676b20204eeee688c
rinox3005 <peppe.rinox@gmail.com>
2024-06-18 18:29:10 +0200

    [UPD] added select to pass value in get and filter hotels

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
 
-// array multidimensionale di hotel con indici numerici che contiene array associativi
+// Array multidimensionale di hotel con indici numerici che contiene array associativi
 $hotels = [
-
     [
         'name' => 'Hotel Belvedere',
         'description' => 'Hotel Belvedere Descrizione',
@@ -38,8 +37,29 @@ $hotels = [
         'vote' => 2,
         'distance_to_center' => 50
     ],
-
 ];
+
+// Filtro gli hotel in base al parcheggio
+$filtered_hotels = [];
+
+// utilizzo isset per verificare se il valore sella select non é nullo, se é settato entro nell'if
+if (isset($_GET['parking'])) {
+
+    // assegno il valore che passo in get ad una variabile
+    $parking_filter = $_GET['parking'];
+
+    // ciclo sull'array multidimensionale e filtro gli hotel in base al valore di $parking_filter che passo in get dalla select
+    foreach ($hotels as $hotel) {
+        if ($parking_filter == 'yes' && $hotel['parking'] == true) {
+            $filtered_hotels[] = $hotel;
+        } elseif ($parking_filter == 'no' && $hotel['parking'] == false) {
+            $filtered_hotels[] = $hotel;
+        }
+    }
+    // se non seleziono nulla l'array da utilizzare é quello di partenza
+} else {
+    $filtered_hotels = $hotels;
+}
 ?>
 
 <!DOCTYPE html>
@@ -57,20 +77,38 @@ $hotels = [
     <main>
         <section class="container mt-5">
             <h1 class="text-center mb-5">PHP Hotels</h1>
+            <!-- Select per i filtri -->
+            <form action="index.php" method="GET">
+                <div class="row justify-content-center mb-3">
+                    <div class="col-6">
+                        <!-- Select per il filtro parcheggio -->
+                        <select class="form-select" aria-label="Default select example" name="parking">
+                            <option selected disabled>Parking</option>
+                            <option value="yes">Yes</option>
+                            <option value="no">No</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <button type="submit" class="btn btn-primary mb-5">Submit</button>
+                </div>
+            </form>
+
+            <h2 class="text-center mb-4">Available Hotels</h2>
             <!-- Tabella -->
             <table class="table">
                 <thead>
                     <tr>
                         <th scope="col"></th>
                         <!-- Ciclo sull'array multidimensionale in posizione 0 per prendere tutte le chiavi dell'array associativo -->
-                        <?php foreach ($hotels[0] as $key => $value) : ?>
+                        <?php foreach ($filtered_hotels[0] as $key => $value) : ?>
                             <th scope="col"><?php echo $key; ?></th>
                         <?php endforeach ?>
                     </tr>
                 </thead>
                 <tbody>
-                    <!-- Ciclo sull'array multidimensionale prendendo anche l'index per calcolare il numero della row dinamicamente -->
-                    <?php foreach ($hotels as $index => $hotel) : ?>
+                    <!-- Ciclo sull'array filtrato o quello di partenza prendendo anche l'index per calcolare il numero della row dinamicamente -->
+                    <?php foreach ($filtered_hotels as $index => $hotel) : ?>
                         <tr>
                             <!-- Rappresento dinamicamente il numero della row e lo incremento ad ogni ciclo -->
                             <th scope="row"><?php echo $index + 1; ?></th>
@@ -87,7 +125,6 @@ $hotels = [
                                         } else {
                                             echo 'No';
                                         }
-                                        // per qualunque altro valore diverso da booleano stampo il valore stesso
                                     } else {
                                         echo $value;
                                     }
