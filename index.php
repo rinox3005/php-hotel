1af09a59717757c69f3c6a9d9eb250c4782bd53e
rinox3005 <peppe.rinox@gmail.com>
2024-06-18 15:04:14 +0200

    [UPD] added boostrap, dynamically populated thead with array values

--- index.php
+++ index.php
@@ -49,27 +49,67 @@ $hotels = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotels</title>
+    <!-- Bootsrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
     <main>
-        <?php foreach ($hotels as $hotel) {
-            foreach ($hotel as $key => $value) {
-                echo "$key - ";
-                if (is_bool($value)) {
-                    if ($value == true) {
-                        echo "Yes";
-                    } else {
-                        echo "No";
-                    }
-                } else {
-                    echo $value;
-                }
-                echo '<br>';
-            }
-            echo '<hr>';
-        }
-        ?>
+        <?php foreach ($hotels as $hotel) ?>
+        <section class="container mt-5">
+            <h1 class="text-center mb-5">PHP Hotels</h1>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col"></th>
+                        <?php foreach ($hotel as $key => $value) : ?>
+                            <th scope="col"><?php echo $key ?></th>
+                        <?php endforeach ?>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <?php foreach ($hotel as $key => $value) : ?>
+                            <td scope="col"><?php echo $value ?></td>
+                        <?php endforeach ?>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        <td>@fat</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Larry the Bird</td>
+                        <td>@twitter</td>
+                        <td>@twitter</td>
+                        <td>@twitter</td>
+                        <td>@twitter</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">4</th>
+                        <td>Larry the Bird</td>
+                        <td>@twitter</td>
+                        <td>@twitter</td>
+                        <td>@twitter</td>
+                        <td>@twitter</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">5</th>
+                        <td>Larry the Bird</td>
+                        <td>@twitter</td>
+                        <td>@twitter</td>
+                        <td>@twitter</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
+
     </main>
 </body>
 
